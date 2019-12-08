a7d45432a9e1fae99c9189a9f8aaeb62f6792c2a
Shravan Nageswaran <shravannageswaran@dhcp-10-250-156-197.harvard.edu>
2019-12-07 21:31:56 -0500

    Starting melia merge

--- js/main-html.js
+++ js/main-html.js
@@ -46,22 +46,6 @@ jQuery(document).ready(function($) {
       
     })
 
-    var owl = $("#owl-testimonials");
-
-      owl.owlCarousel({
-        
-        pagination : true,
-        paginationNumbers: false,
-        autoPlay: 6000, //Set AutoPlay to 3 seconds
-        items : 3, //10 items above 1000px browser width
-        itemsDesktop : [1000,3], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,2], // betweem 900px and 601px
-        itemsTablet: [600,1], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-        
-    });
-
-
 });
 
 // Load CSV file
---
+++ js/main-world.js
@@ -0,0 +1,82 @@
+var colorScale = d3.scaleOrdinal(d3.schemeCategory20);
+
+var methaneChart;
+
+var worldChart;
+
+var mapData;
+
+var worldMeatData;
+
+var WorldVis;
+
+
+
+
+//loadData();
+
+d3.queue()
+    .defer(d3.json, "data/world2.json")
+    .defer(d3.csv, "data/world_meat_consumption.csv")
+    .await(loadWorldData);
+
+function loadWorldData(error, data1, data2) {
+
+    worldDict = {};
+
+    yearDict = {}
+
+    mapData = data1;
+
+    console.log(data1);
+
+
+    //worldDict is a dictionary of dictionaries, where countries are first keys and
+    //years are second keys with metric tons of animals produced as the value
+    worldMeatData = data2;
+
+    console.log(data2);
+
+    worldMeatData.forEach(function(d){
+        d["Year"] = +d["Year"];
+        d["Value"] = +d["Value"];
+
+        if (d.Year == 2017){
+            console.log("Yes")
+            yearDict[d.Area] = d.Value;
+        }
+
+        if (d.Area in worldDict) {
+            worldDict[d.Area][d.Year] = d.Value;
+        }
+        else{
+            worldDict[d.Area] = {};
+            worldDict[d.Area][d.Year] = d.Value ;
+
+        }
+    });
+
+
+
+    console.log(worldMeatData);
+
+    console.log(yearDict);
+    console.log(worldDict);
+
+    worldChart = new WorldVis("world-vis", yearDict, mapData);
+
+}
+
+// d3.json("data/world.json", function(error, json) {
+//     //worldDict is a dictionary of dictionaries, where countries are first keys and
+//     //years are second keys with metric tons of animals produced as the value
+//     mapData = json;
+//
+//
+// });
+
+
+// Render visualization
+function updateVisualization() {
+
+}
